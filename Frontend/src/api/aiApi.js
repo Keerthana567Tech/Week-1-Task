cc2b76d0069a3930c47f5b321bb01b2a411e9c31
Keerthana567Tech <keerthanapilli3@gmail.com>
2025-12-24 11:20:24 +0530

    Update aiApi.js

--- Frontend/src/api/aiApi.js
+++ Frontend/src/api/aiApi.js
@@ -1,6 +1,21 @@
 import axios from "axios";
 import { BASE_URL } from "../constants/config";
+import { handleApiError } from "./errorHandler";
 
-export const askAI = (query) =>
-  axios.post(`${BASE_URL}/ai-search`, { query })
-       .then(res => res.data.answer);
+const API = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const askAI = async (query) => {
+  try {
+    const response = await API.post("/ai-search", { query });
+
+    if (!response.data || !response.data.answer) {
+      throw new Error("NO_CONTEXT");
+    }
+
+    return response.data.answer;
+  } catch (error) {
+    throw new Error(handleApiError(error));
+  }
+};
